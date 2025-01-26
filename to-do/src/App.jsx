fac0ed746e5758263ea8a9515e7d754673b2308f
ShivamHarba <shivamharba.nbnssoe.comp@gmail.com>
2025-01-26 17:59:56 +0530

    code added

--- to-do/src/App.jsx
+++ to-do/src/App.jsx
@@ -9,13 +9,20 @@
 // import NotFound from './Components/NotFound';
 // import DetailInfo from './Components/DetailInfo';
 // import Login from './Components/login';
+import {Provider} from 'react-redux';
+import store from './ReduxContainer/Store'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
    <>
+   <Provider store={store}>
    <div className="h-screen bg-slate-500 dark:bg-golden"></div>
+   </Provider>
+   <Provider store={store}>
+   <div className="h-screen bg-slate-500 dark:bg-golden"></div>
+   </Provider>
 
 
    
---
+++ to-do/src/ReduxContainer/Action.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { buyBook } from './Maker'
+
+const Action = () => {
+  return {
+    type : buyBook
+  }
+    
+  
+}
+
+export default Action
